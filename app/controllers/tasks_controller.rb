adda56078ca258925672d65a28d42bf516f573ef
keiji77 <57708959+keiji77@users.noreply.github.com>
2020-04-29 16:22:52 +0900

    first commit

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,41 @@
+class TasksController < ApplicationController
+  before_action :move_to_index, except: :index
+
+  def index
+    @tasks = Task.all
+  end
+    
+  def new
+    @task = Task.new
+  end
+
+  def create
+    Task.create(task_params)
+    redirect_to action: :index
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update(task_params)
+    redirect_to action: :index
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    redirect_to action: :index
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
+
+  private
+    def task_params
+      params.require(:task).permit(:name, :time)
+    end
+end
